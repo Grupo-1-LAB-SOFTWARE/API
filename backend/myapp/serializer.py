aab9db5e342636bd4af9dd28827ae6c3f6b17459
Reynan Vieira <reynanvt@gmail.com>
2024-01-21 12:35:46 -0300

    serializers criados #22

--- backend/myapp/serializer.py
+++ backend/myapp/serializer.py
@@ -1,2 +1,26 @@
 from rest_framework import serializers
-from myapp.models import Usuario
+from myapp.models import Usuario, Docente, AtividadeLetiva, AtividadePedagogicaComplementar, AtividadeOrientacao
+
+class DocenteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Docente
+        fields = '__all__' 
+
+class AtividadeLetivaSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = AtividadeLetiva
+        fields = '__all__'
+
+class AtividadePedagogicaComplementarSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = AtividadePedagogicaComplementar
+        fields = '__all__'
+
+class AtividadeOrientacaoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = AtividadeOrientacao
+        fields = '__all__'
