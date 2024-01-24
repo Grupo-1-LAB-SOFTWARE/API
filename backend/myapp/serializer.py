8abc195d7b4466015237b0aacd0d944a5989dc87
Amanda Lima Reis <121275873+amandalreis@users.noreply.github.com>
2024-01-24 17:56:43 -0300

    serializers + isEmailConfirmado (#34)

--- backend/myapp/serializer.py
+++ backend/myapp/serializer.py
@@ -1,5 +1,46 @@
 from rest_framework import serializers
-from myapp.models import Usuario, Docente, AtividadeLetiva, AtividadePedagogicaComplementar, AtividadeOrientacao
+from django.contrib.auth.hashers import make_password
+from myapp.models import Usuario, Docente, AtividadeLetiva, AtividadePedagogicaComplementar, AtividadeOrientacao, Campus, Instituto, Curso
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    Senha = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = Usuario
+        fields = ('Id', 'Login', 'NomeCompleto', 'Perfil', 'DataCadastro', 'Email', 'isEmailConfirmado')
+        extra_kwargs = {'Senha': {'write_only': True}}
+    
+    def create(self, validated_data):
+        user = Usuario.objects.create(
+            Id=validated_data['Id'],
+            Login=validated_data['Login'],
+            NomeCompleto=validated_data['NomeCompleto'],
+            Perfil=validated_data['Perfil'],
+            DataCadastro=validated_data['DataCadastro'],
+            Email=validated_data['Email'],
+            isEmailConfirmado=validated_data['isEmailConfirmado'],
+            Senha = make_password(validated_data['Senha'])
+        )
+        user.save()
+        return user
+
+class CampusSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Campus
+        fields = '__all__' 
+
+class InstitutoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Instituto
+        fields = '__all__' 
+
+class CursoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Curso
+        fields = '__all__' 
 
 class DocenteSerializer(serializers.ModelSerializer):
 
