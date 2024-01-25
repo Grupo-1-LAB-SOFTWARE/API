ad7b4483e09f3077f7423f7f9b74de67f63cb67b
Reynan Vieira <102336632+ReynanVv@users.noreply.github.com>
2024-01-25 00:37:45 -0300

    View de enviou e confirmação de e-mail (#36) (#37)

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes,force_str
+from django.contrib.sites.shortcuts import get_current_site
+from myapp.serializer import UsuarioSerializer
+from myapp.models import Usuario
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class UsuarioView(APIView):
+    def post(self, request):
+        serializer = UsuarioSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save(commit=False)  # Cria o usuário mas não salva
+            token = default_token_generator.make_token(user)
+            mail_subject = 'Ative sua conta.'
+            message = render_to_string('acc_active_email.html', {
+                'user': user,
+                'domain': get_current_site(request).domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': token,
+            })
+            send_mail(mail_subject, message, 'meu_email@example.com', [user.Email])
+            return Response("Por favor confirme seu email para completar o registro.")
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class AtivarContaView(APIView):
+    def get(self, request, uidb64, token):
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = Usuario.objects.get(pk=uid)
+            if default_token_generator.check_token(user, token):
+                user.isEmailConfirmado = True
+                user.save()  # Salva o usuário
+                return Response("Obrigado pela sua confirmação de email. Agora você pode fazer login na sua conta.")
+            else:
+                return Response("O link de ativação é inválido!")
+        except(TypeError, ValueError, OverflowError, Usuario.DoesNotExist):
+            user = None
+            return Response("O link de ativação é inválido!")
+        
