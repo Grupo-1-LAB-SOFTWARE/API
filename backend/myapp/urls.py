18bd6a6c178ddbf7a60751a5d79c2a6e8de5054c
Reynan Vieira <102336632+ReynanVv@users.noreply.github.com>
2024-01-29 02:18:41 -0300

    Envio e Ativação de Conta (#45)

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import UsuarioView, AtivarContaView, UsuariosTesteView
+from .views import UsuarioView, ActivateEmail
 
 urlpatterns = [
-    path('usuarios/', UsuariosTesteView.as_view(), name='usuarios'),
-    path('usuarios/<int:user_id>/', UsuariosTesteView.as_view(), name='usuarios_get_by_id'),
+    path('usuarios/<int:user_id>/', UsuarioView.as_view(), name='usuarios_get_by_id'),
     path('registrar/', UsuarioView.as_view(), name='registrar'),
-    path('ativar/<uidb64>/<token>/', AtivarContaView.as_view(), name='ativar_conta'),
+    path('activate/<str:login>/', ActivateEmail.as_view(), name='activate'),
 ]
