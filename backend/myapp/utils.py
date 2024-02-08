aa9ae9f234e23f29c1162be2bf8b52eb51a4fa6e
Amanda Lima Reis <121275873+amandalreis@users.noreply.github.com>
2024-02-08 15:51:14 -0300

    Amanda dev (#67) (#68)
    * serializers + isEmailConfirmado
    
    * serializers + isEmailConfirmado (#34)
    
    * getAllView+mudando models+retirada do commit=False
    
    * gets e post UsuariosTesteView
    
    * view update
    
    * #23
    
    * #23 #24 #25 #26
    
    * LoginView
    
    * mudando de id pra pk
    
    * Readme e venv
    
    * Remover venv do repositório
    
    * Atualizando README
    
    * commit save
    
    * ajustes README
    
    * view login finalizada
    
    * Correção de versões das Libs e .env resolvida (#62)
    
    * correção loginview
    
    * D/testenv (#63)
    
    * Correção de versões das Libs e .env resolvida
    
    * .env corrigido
    
    * space
    
    * h
    
    * commit final loginview
    
    * responses_padronizadas
    
    ---------
    
    Co-authored-by: iza <iza.americo@gmail.com>
    Co-authored-by: IzA888 <135436213+IzA888@users.noreply.github.com>
    Co-authored-by: Reynan Vieira <reynanvt@gmail.com>
    Co-authored-by: Reynan Vieira <102336632+ReynanVv@users.noreply.github.com>

--- backend/myapp/utils.py
+++ backend/myapp/utils.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 from bson import ObjectId
 from django.conf import settings
 from decouple import config
+from rest_framework.response import Response
+from rest_framework import status
 
 class Util:
     @staticmethod
@@ -24,3 +26,35 @@ class Util:
         connection = get_connection()
 
         send_mail(subject, message, from_email, recipient_list, connection=connection)
+
+    @staticmethod
+    def response_ok(mensagem_personalizada):
+        return Response({"sucess": f"{mensagem_personalizada}"}, status=status.HTTP_200_OK)
+
+    @staticmethod
+    def response_ok_no_message(mensagem_personalizada):
+        return Response(mensagem_personalizada, status=status.HTTP_200_OK)
+
+    @staticmethod
+    def response_ok_token(chave_token):
+        return Response({"token": f"{chave_token}"}, status=status.HTTP_200_OK)
+
+    @staticmethod
+    def response_created(mensagem_personalizada):
+        return Response(mensagem_personalizada, status=status.HTTP_201_CREATED)
+
+    @staticmethod
+    def response_bad_request(mensagem_personalizada):
+        return Response({"bad_request": f"{mensagem_personalizada}"}, status=status.HTTP_400_BAD_REQUEST)
+
+    @staticmethod
+    def response_not_found(mensagem_personalizada):
+        return Response({"not_found": f"{mensagem_personalizada}"}, status=status.HTTP_404_NOT_FOUND)
+
+    @staticmethod
+    def response_unauthorized(mensagem_personalizada):
+        return Response({"unauthorized": f"{mensagem_personalizada}"}, status=status.HTTP_401_UNAUTHORIZED)
+
+    @staticmethod
+    def response_forbidden(mensagem_personalizada):
+        return Response({"forbidden": f"{mensagem_personalizada}"}, status=status.HTTP_403_FORBIDDEN)
