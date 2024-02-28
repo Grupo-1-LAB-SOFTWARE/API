02a47a243bc62f4aff768a2966733f76aa834fbf
Amanda Lima Reis <121275873+amandalreis@users.noreply.github.com>
2024-02-28 16:22:58 -0300

    put correção (#88)
    * serializers + isEmailConfirmado
    
    * serializers + isEmailConfirmado (#34)
    
    * getAllView+mudando models+retirada do commit=False
    
    * gets e post UsuariosTesteView
    
    * view update
    
    * #23
    
    * #23 #24 #25 #26
    
    * LoginView
    
    * mudando de id pra pk
    
    * Readme e venv
    
    * Remover venv do repositório
    
    * Atualizando README
    
    * commit save
    
    * ajustes README
    
    * view login finalizada
    
    * Correção de versões das Libs e .env resolvida (#62)
    
    * correção loginview
    
    * D/testenv (#63)
    
    * Correção de versões das Libs e .env resolvida
    
    * .env corrigido
    
    * space
    
    * h
    
    * commit final loginview
    
    * responses_padronizadas
    
    * corrigindo versao pymongo e usuario extra
    
    * migrations
    
    * adicionando validacao email e username no post
    
    * validacao email e username em PUT usuario
    
    * mudança para postgreSQL + UsuarioSerializer
    
    * ajeitando update no UsuarioSerializer
    
    * Atualizando README para quem quer desenvolver
    
    * corrigindo erro em put Usuário
    
    ---------
    
    Co-authored-by: iza <iza.americo@gmail.com>
    Co-authored-by: IzA888 <135436213+IzA888@users.noreply.github.com>
    Co-authored-by: Reynan Vieira <reynanvt@gmail.com>
    Co-authored-by: Reynan Vieira <102336632+ReynanVv@users.noreply.github.com>

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -17,7 +17,7 @@ class UsuarioView(APIView):
         else:
             return self.getAll(request)
             
-    def put(self, request, user_id):
+    def put(self, request, user_id=None):
         if user_id is not None:
             try:
                 user = Usuario.objects.get(pk=user_id)
@@ -45,7 +45,9 @@ class UsuarioView(APIView):
                     return Util.response_bad_request(serializer.errors)
 
             except Usuario.DoesNotExist:
-                return Util.response_not_found('Não foi possível encontrar um usuário com o id fornecido')
+                return Util.response_not_found('Não foi possível encontrar um usuário com o id fornecido.')
+
+        return Util.response_bad_request('É necessário fornecer o id do usuário que você deseja atualizar em usuarios/{id}')
 
 
     #Pra pegar todos os usuários, sem especificar id
