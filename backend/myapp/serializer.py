f38b213546f1da469a508475eba4c0c8c22caa98
iza <iza.americo@gmail.com>
2024-03-25 16:44:59 -0300

    add: 'username'+token

--- backend/myapp/serializer.py
+++ backend/myapp/serializer.py
@@ -42,7 +42,7 @@ class CustomizarTokenSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-        #token['username'] = user.username
+        token['username'] = user.username
 
         return token
 
