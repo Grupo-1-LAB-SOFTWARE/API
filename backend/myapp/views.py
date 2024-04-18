822ae81cc31de59f2924817af40142df38373e78
Amanda Lima Reis <121275873+amandalreis@users.noreply.github.com>
2024-04-18 19:56:49 -0300

    Amanda dev (#234)
    * migrations
    
    * correções de duplicações
    
    * Teste
    
    * resolvendo teste
    
    * Estrutura projeto_extensao
    
    * mais alguns serializers e views
    
    * refatoração das views
    
    * mais um componente
    
    * correção user_id delete UsuarioView
    
    * novo componente distribuicao_ch_semanal
    
    * finalizando componentes da estrutura
    
    * documentos comprobatórios
    
    * nome do arquivo pdf corrigido
    
    * reestruturação completa da api
    
    * commit
    
    * Validações views
    
    * lógica atividade_letiva e calculo_ch_semanal_aulas
    
    * adicionando validações
    
    * validacao supervisão acadêmica
    
    * refatorando
    
    * refatoração
    
    * autenticação corrigida
    
    * algumas coisas
    
    * abertura ruim
    
    * começando a adicionar views admin
    
    * endpoints usuario completos
    
    * modificando response login
    
    * correção
    
    * correção
    
    * atividade letiva view completa
    
    * calculochsemanalaulas completo
    
    * atividade pedagógica complementar view
    
    * atividade orientação supervisão preceptoria tutoria view
    
    * DescricaoOrientacaoCoorientacaoAcademicaVIEW
    
    * SupervisaoAcademicaView
    
    * finalizando endpoints de ensino
    
    * retirando views desnecessárias
    
    * corrigindo erros urls
    
    * AvaliacaoDiscenteView
    
    * perfil sempre docente
    
    * lógica cálculo CH SEMANAL DA ATIVIDADE DE ENSINO
    
    * corrigindo alguns erros
    
    * corrigindo problemas
    
    * corrigindo problemas
    
    * corrigindo calculos ch
    
    * ajeitado commit reynan
    
    * PREENCHENDO RADOC SUCCESS
    
    * Facilitador atividade_letiva
    
    * atividade letiva modificada pro front
    
    * corrigindo
    
    * finalizando calculo distribuicao ch semanal
    
    * ajuste nome_usuario
    
    * calculo_ch_semanal_aulas nao zera mais
    
    * corrigindo erros
    
    * add db em produção
    
    * railway.json
    
    * tirando appscript
    
    * adicionando gunicorn
    
    * habilitando nova url da api em prof
    
    * corrigindo
    
    * mostrando perfil em login
    
    * login novo
    
    * fix remover código desnecessário
    
    * testando weasyprint
    
    * modificando railway prod
    
    * modificando railway prod
    
    * mod
    
    * dockerfile
    
    * d
    
    * di
    
    * docke
    
    * dock
    
    * movendo
    
    * corrigindo
    
    * m
    
    * tentativa nixpacks
    
    * botai no backend que resolve
    
    * eu li
    
    * pips
    
    * correções
    
    * correções
    
    * correções
    
    * correção
    
    * correções
    
    * correção
    
    * correcao
    
    * correcao
    
    * co
    
    * mudança pro nixpacks novamente
    
    * add lib
    
    * start
    
    * apt
    
    * nixpacksplan
    
    * pango
    
    * j
    
    * modificações
    
    * corrigindo erro update usuario
    
    * commit
    
    * agora pode docente
    
    * voltando ao localhost
    
    * a
    
    * a
    
    * a
    
    * a

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -276,7 +276,7 @@ class ActivateEmail(APIView):
             return Util.response_not_found('Usuário não encontrado')
         
         # origin = "http://localhost:4200"
-        origin = "https://8dc9-200-129-150-3.ngrok-free.app"
+        origin = "https://f3aa-2804-389-d130-7174-712b-3190-cb44-8b0f.ngrok-free.app"
         return redirect(f'{origin}/login/?ativacao_sucesso=true')
 
 class AtividadeLetivaView(APIView):
